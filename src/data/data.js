40a1e667c7d7b279289e761fe9e1dc80b48107e9
Damon-Jo <lunaticjky@gmail.com>
2023-06-13 22:56:53 -0700

    fixed favicon and title of app, and added one more project in data.js

--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,7 @@ import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
 import LetsPack from '../assets/projects/letspack.png'
 import Instagram from '../assets/projects/instagram.png'
+import Portfolio from '../assets/projects/portfolio.png'
 
 export const data=[
     {
@@ -17,6 +18,13 @@ export const data=[
         image:Instagram,
         github:"https://github.com/Damon-Jo/instagram_clone",
         live:"http://13.52.231.125/main/",
+    },
+    {
+        id:3,
+        name:"React Application",
+        image:Portfolio,
+        github:"https://github.com/Damon-Jo/kwanyong-portfolio",
+        live:"https://kwanyong-damon-app.herokuapp.com/",
     },
 
 
